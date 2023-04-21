3a1311b10b1e17421bac8375db1c6ae5cef415e4
Raphael Marchetti Calciolari <79259612+Raphael-Marchetti-Calciolari@users.noreply.github.com>
2023-04-21 11:11:45 -0300

    Using MovieService into MoviesController

--- src/main/java/marchetti/raphael/movies/controllers/MoviesController.java
+++ src/main/java/marchetti/raphael/movies/controllers/MoviesController.java
@@ -1,16 +1,26 @@
 package marchetti.raphael.movies.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import marchetti.raphael.movies.models.Movie;
+import marchetti.raphael.movies.services.MovieService;
+
 @RestController
 @RequestMapping("/api/v1/movies") // this controller will handle requests in this endpoint
 public class MoviesController {
+
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping
-    public ResponseEntity<String> getAllMovies() {
-        return new ResponseEntity<String>("All Movies", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 }
