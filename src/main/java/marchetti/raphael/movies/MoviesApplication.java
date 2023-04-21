f9f09ab5fc54292581ea772ecf278032e0a55eef
Raphael Marchetti Calciolari <79259612+Raphael-Marchetti-Calciolari@users.noreply.github.com>
2023-04-20 23:14:08 -0300

    First commit

---
+++ src/main/java/marchetti/raphael/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package marchetti.raphael.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
