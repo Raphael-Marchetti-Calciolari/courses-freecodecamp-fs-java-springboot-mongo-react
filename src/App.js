504aced258a16e5e53ab1c2b551329a23556fac6
Raphael Marchetti Calciolari <79259612+Raphael-Marchetti-Calciolari@users.noreply.github.com>
2023-04-21 17:37:50 -0300

    Removed debug log

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   const getMovies = async () => {
     try{
       const response = await api.get('/api/v1/movies');
-      console.log(response.data)
       setMovies(response.data);
     }
     catch (err){
