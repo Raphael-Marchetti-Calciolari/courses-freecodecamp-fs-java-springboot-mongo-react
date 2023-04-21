9a2e3c4d43293c36276b4268bc0e4598bfc1a6d8
Raphael Marchetti Calciolari <79259612+Raphael-Marchetti-Calciolari@users.noreply.github.com>
2023-04-21 17:12:45 -0300

    Oppened CORS for communicating with front-end

--- src/main/java/marchetti/raphael/movies/controllers/ReviewsController.java
+++ src/main/java/marchetti/raphael/movies/controllers/ReviewsController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import marchetti.raphael.movies.services.ReviewService;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
+@CrossOrigin(origins = "*")
 public class ReviewsController {
 
     @Autowired
