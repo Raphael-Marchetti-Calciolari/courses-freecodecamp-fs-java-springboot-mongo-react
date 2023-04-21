9cd82d3d57086c608b2442d352e3f7079f878900
Raphael Marchetti Calciolari <79259612+Raphael-Marchetti-Calciolari@users.noreply.github.com>
2023-04-21 11:28:11 -0300

    Added new logic to get only one movie to the controller

--- src/main/java/marchetti/raphael/movies/controllers/MoviesController.java
+++ src/main/java/marchetti/raphael/movies/controllers/MoviesController.java
@@ -1,11 +1,14 @@
 package marchetti.raphael.movies.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +23,12 @@ public class MoviesController {
     private MovieService movieService;
 
     @GetMapping
-    public ResponseEntity<List<Movie>> getAllMovies() {
+    public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}") // receives a param that is the id of the movie
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id){ // whatever is get through the path variable, it is converted to ObjectId id
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
     }
 }
