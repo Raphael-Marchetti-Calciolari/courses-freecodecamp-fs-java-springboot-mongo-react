7897dbb51cdd3a3de90c5b636b9dcd66e89a18b9
Raphael Marchetti Calciolari <79259612+Raphael-Marchetti-Calciolari@users.noreply.github.com>
2023-04-21 19:03:32 -0300

    Added css variable in Hero.js and beginning of syling Hero.css
    The css in-line made possible to use a variable to store the image url at the Hero.css file

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -6,13 +6,13 @@ import { Paper } from '@mui/material';
 const Hero = ({movies}) => {
   return (
     <div>
-        <Carousel>
+        <Carousel className='movie-carousel-container'>
         {
             movies?.map((movie) => {
                 return (
                     <Paper>
                         <div className="movie-card-container">
-                            <div className="movie-card">
+                            <div className="movie-card" style={{"--imgu": `url(${movie.backdrops[0]})`}}>
                                 <div className="movie-detail">
                                     <div className="movie-poster">
                                         <img src={movie.poster} alt="" />
